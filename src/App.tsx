972de24901d2c15fd991a3a2965334fea69f8eb8
Shafquat <sassquatch.nz@gmail.com>
2025-08-03 06:50:56 +1000

    Replace terminal with silk

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,18 @@
 import React from 'react';
-import FaultyTerminal from './components/FaultyTerminal';
+import Iridescence from './components/Iridescence';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <div style={{ width: '100%', height: '100vh', position: 'relative' }}>
-        <FaultyTerminal
-          scale={2}
-          gridMul={[2, 1]}
-          digitSize={1.2}
-          timeScale={.015}
-          pause={false}
-          scanlineIntensity={.1}
-          glitchAmount={1}
-          flickerAmount={1}
-          noiseAmp={1}
-          chromaticAberration={0}
-          dither={0}
-          curvature={0.1}
-          tint="#ffd32a"
-          mouseReact={true}
-          mouseStrength={0.2}
-          pageLoadAnimation={true}
-          brightness={0.8}
-        />
+      <Iridescence
+        color={[0.8, 0, 0.2]}
+        mouseReact={false}
+        amplitude={0.1}
+        speed={0.4}
+      />
+      <div className="content">
+        {/* Your new content will go here */}
       </div>
     </div>
   );
