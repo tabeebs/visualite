9a5bd7e76089a168895c6cfc7bfb652c780ab2fa
Shafquat <sassquatch.nz@gmail.com>
2025-08-03 06:41:39 +1000

    Replace grid with terminal

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,29 @@
 import React from 'react';
-import DotGrid from './components/DotGrid';
+import FaultyTerminal from './components/FaultyTerminal';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <div style={{ width: '100%', height: '100vh', position: 'relative' }}>
-        <DotGrid
-          dotSize={5}
-          gap={15}
-          baseColor="#271e37"
-          activeColor="#2417d9"
-          proximity={120}
-          shockRadius={250}
-          shockStrength={5}
-          resistance={750}
-          returnDuration={1.5}
+        <FaultyTerminal
+          scale={2}
+          gridMul={[2, 1]}
+          digitSize={1.2}
+          timeScale={.015}
+          pause={false}
+          scanlineIntensity={.1}
+          glitchAmount={1}
+          flickerAmount={1}
+          noiseAmp={1}
+          chromaticAberration={0}
+          dither={0}
+          curvature={0.1}
+          tint="#ffd32a"
+          mouseReact={true}
+          mouseStrength={0.2}
+          pageLoadAnimation={true}
+          brightness={0.8}
         />
       </div>
     </div>
